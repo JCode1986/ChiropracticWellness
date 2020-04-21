8deab563acecc8f8e6f94dd02d6235a8b2569edf
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-04-20 23:07:25 -0700

    MVC scaffold complete

--- ECommerceApp/Startup.cs
+++ ECommerceApp/Startup.cs
@@ -16,6 +16,7 @@ namespace ECommerceApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,12 +29,11 @@ namespace ECommerceApp
 
             app.UseRouting();
 
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
