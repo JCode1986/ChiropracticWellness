72fcc388fdd1b6851ee0e797fbb2d037f0550967
Sue Tarazi <suemachtley@gmail.com>
2020-04-21 16:50:51 -0700

    added some post stuff

---
+++ ECommerceApp/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerceApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ECommerceApp.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        [BindProperty]
+        public RegisterInput RegisterData { get; set; }
+
+        public RegisterModel (UserManager<ApplicationUser> usermanager, SignInManager<ApplicationUser> signIn)
+        {
+            _userManager = usermanager;
+            _signInManager = signIn;
+
+        }
+        public void OnGet()
+        {
+
+        }
+
+        public class RegisterInput
+        {
+
+        }
+    }
+}
--- ECommerceApp/Startup.cs
+++ ECommerceApp/Startup.cs
@@ -30,7 +30,8 @@ namespace ECommerceApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            //services.AddMvc();
+            services.AddRazorPages();
             //registering the database
             services.AddDbContext<ApplicationDbContext>(options =>
             {
@@ -58,6 +59,7 @@ namespace ECommerceApp
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
