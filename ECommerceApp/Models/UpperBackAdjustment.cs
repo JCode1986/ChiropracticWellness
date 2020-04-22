634788e7aa6a6a49bc5705c7cfa6395d4379ec97
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-04-21 17:38:54 -0700

    10 Models created

---
+++ ECommerceApp/Models/UpperBackAdjustment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Models
+{
+    public class UpperBackAdjustment
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Duration { get; set; }
+    }
+}
