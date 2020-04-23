4d6cd44fa0c02173e36146f96e7654ac4309d0e8
Sue Tarazi <suemachtley@gmail.com>
2020-04-23 16:11:58 -0700

    updated store db seeding with images

--- ECommerceApp/Data/StoreDbContext.cs
+++ ECommerceApp/Data/StoreDbContext.cs
@@ -25,7 +25,7 @@ namespace ECommerceApp.Data
                     Description = "Initial Evaluation where the Chiropractor will spend time with you to learn about your condition, your previous level of function, and how your condition is affecting your life. He will then perform an examination and perform a treatment.",
                     Price = 250.00M,
                     Duration = "1 hour",
-                    Image = "~/Assets/.jpg"
+                    Image = "~/Assets/InitialEvaluation.jpg"
                 },
                 new Inventory
                 {
@@ -33,7 +33,8 @@ namespace ECommerceApp.Data
                     ServiceType = "Follow-up Consultation",
                     Description = "A Follow-up Evaluation where the Chiropractor will spend time with you to see how you are doing and how you are meeting your functional goals. He will then perform an mini-examination and perform a treatment. Note: This service is only available after you have had a full initial consultation",
                     Price = 150.00M,
-                    Duration = "45-60 minutes"
+                    Duration = "45-60 minutes",
+                    Image = "~/Assets/FollowUpConsultation.jpg"
                 },
                 new Inventory
                 {
@@ -41,7 +42,8 @@ namespace ECommerceApp.Data
                     ServiceType = "Lower Back Adjustment",
                     Description = "A follow up visit for targetted adjustment of the lower back.",
                     Price = 75.00M,
-                    Duration = "30 minutes"
+                    Duration = "30 minutes",
+                    Image = "~/Assets/LowBack1.jpg"
                 },
                 new Inventory
                 {
@@ -49,7 +51,8 @@ namespace ECommerceApp.Data
                     ServiceType = "Neck Adjustment",
                     Description = "A follow up visit for targetted adjustment of the neck.",
                     Price = 85.00M,
-                    Duration = "45 minutes"
+                    Duration = "45 minutes",
+                    Image = "~/Assets/NeckAdjustment1.jpg"
                 },
                 new Inventory
                 {
@@ -57,7 +60,8 @@ namespace ECommerceApp.Data
                     ServiceType = "Upper Back Adjustment",
                     Description = "A follow up visit for targetted adjustment of the upper back/thoracic region.",
                     Price = 75.00M,
-                    Duration = "30 minutes"
+                    Duration = "30 minutes",
+                    Image = "~/Assets/UpperBack1.jpg"
                 },
                 new Inventory
                 {
@@ -65,7 +69,8 @@ namespace ECommerceApp.Data
                     ServiceType = "5-Pack of Lower Back Adjustments",
                     Description = "Reguar adjustments are sometimes necessary to maintain pain free postural alignment. This service allows you to purchase 5 low back adjustments at a discounted rate that may be used within the next 6 months.",
                     Price = 300.00M,
-                    Duration = "30 minutes each"
+                    Duration = "30 minutes each",
+                    Image = "~/Assets/LowBack2.jpg"
                 },
                 new Inventory
                 {
@@ -73,7 +78,8 @@ namespace ECommerceApp.Data
                     ServiceType = "Pack of Neck Adjustments",
                     Description = "Reguar adjustments are sometimes necessary to maintain pain free postural alignment. This service allows you to purchase 5 neck adjustments at a discounted rate that may be used within the next 6 months.",
                     Price = 325.00M,
-                    Duration = "1 hour 30 minutes"
+                    Duration = "1 hour 30 minutes",
+                    Image = "~/Assets/NeckAdjustment2.jpg"
                 },
                 new Inventory
                 {
@@ -81,7 +87,8 @@ namespace ECommerceApp.Data
                     ServiceType = "Pack of Upper Back Adjustments",
                     Description = "Reguar adjustments are sometimes necessary to maintain pain free postural alignment. This service allows you to purchase 5 upper back/thoracic region adjustments at a discounted rate that may be used within the next 6 months.",
                     Price = 100.00M,
-                    Duration = "1 hour 30 minutes"
+                    Duration = "1 hour 30 minutes",
+                    Image = "~/Assets/UpperBack2.jpg"
                 },
                 new Inventory
                 {
@@ -89,7 +96,8 @@ namespace ECommerceApp.Data
                     ServiceType = "60 Minute Massage",
                     Description = "A 60-minute Orthopedic massage allowing for deep tissue therapy of problem areas. The massage may incorporate Shiatzu, Swedish or trigger point techniques.",
                     Price = 80.00M,
-                    Duration = "60 minutes"
+                    Duration = "60 minutes",
+                    Image = "~/Assets/Massage1.jpg"
                 },
                 new Inventory
                 {
@@ -97,7 +105,8 @@ namespace ECommerceApp.Data
                     ServiceType = "30 Minute Massage",
                     Description = "A 30-minute Orthopedic massage providing an overall massage. The massage may incorporate Shiatzu or Swedish techniques. This massage is best suited before a Chiropractic adjustment. If you are seeking deep tissue or have a specific problem area, consider a 60-minute massage instead.",
                     Price = 45.00M,
-                    Duration = "30 minutes"
+                    Duration = "30 minutes",
+                    Image = "~/Assets/Massage2.jpg"
                 }
             );
         }
