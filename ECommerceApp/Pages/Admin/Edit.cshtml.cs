f03f74743ee329c49695ca51d15b654f5e5c65b1
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-05-01 21:37:30 -0700

    edit razor page added

---
+++ ECommerceApp/Pages/Admin/Edit.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerceApp.Models;
+using ECommerceApp.Models.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ECommerceApp.Pages.Admin
+{
+    public class EditModel : PageModel
+    {
+        private ICartItems _cart;
+
+        public EditModel(ICartItems cart)
+        {
+            _cart = cart;
+        }
+
+        public CartItems CartItems { get; set; }
+        public async Task<IActionResult> OnGet(int cartItemID)
+        {
+            CartItems = await _cart.GetItemByID(cartItemID);
+            return Page();
+        }
+    }
+}
