82a67e2535758b7430a65c480b545395116397ff
Sue Tarazi <suemachtley@gmail.com>
2020-05-03 16:46:09 -0700

    Create Admin form now working :)

--- ECommerceApp/Pages/Admin/ManageServices.cshtml.cs
+++ ECommerceApp/Pages/Admin/ManageServices.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ECommerceApp.Models;
 using ECommerceApp.Models.Interface;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -21,6 +22,8 @@ namespace ECommerceApp.Pages.Admin
         }
 
         public List<Inventory> Services { get; set; }
+
+        public IFormFile Photo { get; set; }
 
         [BindProperty]
         public Inventory Service { get; set; }
